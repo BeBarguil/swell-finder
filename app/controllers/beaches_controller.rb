a9f4db9e11ccdc369a0dd5df7224d16d8f27e8ee
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-03-05 20:27:32 -0300

    Add distance filter with dropdown (50km, 100km, 200km, 500km)

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,5 +1,7 @@
 class BeachesController < ApplicationController
   def index
+    @max_distance = params[:distance].present? ? params[:distance].to_i : nil
+    
     if params[:lat].present? && params[:lng].present?
       user_lat = params[:lat].to_f
       user_lng = params[:lng].to_f
@@ -7,18 +9,23 @@ class BeachesController < ApplicationController
       # Buscar todas as praias e calcular distância
       all_beaches = Beach.all.map do |beach|
         distance = beach.distance_from(user_lat, user_lng)
-        [beach, distance]
+        { beach: beach, distance: distance }
       end
       
-      # Ordenar por distância e pegar as 9 mais próximas
-      @beaches = all_beaches
-        .sort_by { |beach, distance| distance }
-        .first(9)
-        .map { |beach, distance| beach }
+      # Filtrar por distância se especificado
+      if @max_distance
+        all_beaches = all_beaches.select { |b| b[:distance] <= @max_distance }
+      end
+      
+      # Ordenar por distância
+      @beaches_with_distance = all_beaches.sort_by { |b| b[:distance] }
+      @beaches = @beaches_with_distance.map { |b| b[:beach] }
       
       @user_location = { lat: user_lat, lng: user_lng }
+      @total_found = @beaches.count
     else
       @beaches = Beach.all.limit(9)
+      @beaches_with_distance = []
     end
   end
 
