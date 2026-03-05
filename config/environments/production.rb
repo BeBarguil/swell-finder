67b37468f830e6e463b3a9f76fdb6973ae495b3a
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-03-05 19:57:05 -0300

    Show detailed errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.enable_reloading = false
   config.eager_load = true
-  config.consider_all_requests_local = false
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.compile = false
-  config.active_storage.service = :local
-  config.log_level = :info
-  config.log_tags = [ :request_id ]
-  config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
+  config.consider_all_requests_local = true
+  config.public_file_server.enabled = true
+  config.assets.compile = true
+  config.log_level = :debug
   config.active_support.report_deprecations = false
-  config.active_record.dump_schema_after_migration = false
 end
